84e64f0842fde736b76bd5dc38b4d6c7a6d2bf24
Piotr Suchy <piotrsuchy6@tlen.pl>
2023-04-27 14:46:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Go CRUD API Basic Project
+
+In this repo I've put the code for a simple CRUD API made with Golang. All the code is within the main.go file and there's no frontend, however I've tested it with POSTMAN API calls:
+
+## GET ALL
+
+A screenshot from postman after sending a GET ALL get request:
+![GET ALL](media/POSTMAN_GET_ALL.png)
+
+## CREATE
+
+A screenshot from postman after sending a CREATE get request:
+![CREATE](media/POSTMAN_CREATE.png)
+
+A screenshot of GET_ALL after creating with the earlier request:
+![AFTER CREATE](media/POSTMAN_AFTER_CREATE.png)
+
+As you can see, there's a new entry.
+
+## UPDATE
+
+A screenshot from postman after sending a UPDATE put request:
+![UPDATE](media/POSTMAN_UPDATE.png)
+
+The value for the director has been changed.
+
+## DELETE
+
+A screenshot from postman after sending a DELETE del request:
+![DELETE](media/POSTMAN_DEL.png)
+
+Any entry with an ID in the request would be deleted.
+
+## Final Thoughts
+
+This is my first project in Go.
+This repo was created while watching a tutorial about Go:
+[Learn Go Programming](https://www.youtube.com/watch?v=jFfo23yIWac)
---
+++ src/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+// struct of type movie and directors:
+type Movie struct {
+	ID    string `json:"id"`
+	Isbn  string `json:"isbn"`
+	Title string `json:"title"`
+	// pointer to Director
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	// encoding into json
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			// append all the other data in the place of the movie we want to delete
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	// using _ for a variable we won't use
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	// creating a random value from that range
+	movie.ID = strconv.Itoa(rand.Intn(10000000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	// this is not the way to do it when working with databases
+	// but for this case it is a possible approach
+
+	// set json content type
+	w.Header().Set("Content-Type", "application/json")
+
+	// params
+	params := mux.Vars(r)
+	// loop over the movies, range
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			// delete the movie with the id youve sent
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			// add a new movie - the movie that we send in the body of postman
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "438312", Title: "Movie Two", Director: &Director{Firstname: "Steven", Lastname: "Smith"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
